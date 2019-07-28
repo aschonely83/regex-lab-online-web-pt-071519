4422e9e821f5029c874ed047d2ff42ab69b7a699
Adam Schonely <aschonely@gmail.com>
2019-07-28 22:45:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -19,7 +19,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/\d\d\d-\d\d\d-\d\d\d\d/) then
+  if phone.match(/\d{3}/) then
     return true
   else
     return false
